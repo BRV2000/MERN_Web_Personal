54397adcf3a20070f9e96b4ae0193664a333e8e2
BRV2000 <branrv2000@gmail.com>
2023-06-21 19:44:02 -0600

    desarrollo de la parte de user controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,50 @@
+const user = require("../models/user");
+const bcrypt = require("bcryptjs");
+
 async function getMe(req,res){
-    res.status(200).send({msg:"Todo OK"});
+   
+   const {user_id} = req.user;
+   
+   const response = await user.findById(user_id);
+
+   if (!response) {
+    res.status(400).send({msg: "No se ha encontrado usuario"});
+   } else {
+    res.status(200).send(response);
+   }
+   
+   
+}
+
+async function getUsers(req,res){
+    const {active} = req.query;
+    let response = null;
+
+    if (active === undefined) {
+        response = await user.find();
+    } else {
+        response = await user.find({active});
+    }
+    
+    res.status(200).send(response);
+}
+
+async function createUser(req, res){
+   const { password } = req.body;
+   
+   const salt = bcrypt.genSaltSync(10);
+   const hasPassword = bcrypt.hashSync(password,salt);
+
+   const user = new User({ ...req.body, active: false, password: hasPassword});
+    
+
+    console.log(user);
+   
+    res.status(200).send({msg:"ok creando usuarios"});
 }
 
 module.exports = {
     getMe,
+    getUsers,
+    createUser,
 };
